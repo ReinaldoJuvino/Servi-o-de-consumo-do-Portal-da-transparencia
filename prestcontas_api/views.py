17835d7e047a0af1f326d8cfb5dfdf686b0f4bc6
Igor Xavier <igoorlimaa@gmail.com>
2020-11-13 10:01:02 -0300

    atualizacao

--- prestcontas_api/views.py
+++ prestcontas_api/views.py
@@ -8,68 +8,67 @@ from prestcontas_api.models import Entidade, Instrumento, TipoInstrumento, TipoO
 
 
 def index(request):
-    get_entidade = Entidade.objects.all()
+    entidades = Entidade.objects.all()
     headers = {'chave-api-dados': '99d565aeacc2e7414dadd7a377f17417', 'Accept': "*/*"}
     url_base = 'http://www.portaltransparencia.gov.br/api-de-dados'
 
-    for entidade in get_entidade:
+    for entidade in entidades:
         pagina = 1
         while pagina is not None:
             payload_all_convenios = f'?codigoIBGE={entidade.codigo_municipio}&pagina={pagina}'
-            sucess = True
-            while sucess:
+            success = True
+            while success:
                 try:
                     res = requests.get(url_base + '/convenios' + payload_all_convenios, headers=headers)
                     if res.json():
-                        # print('passou')
-                        # print(entidade)
-                        # pagina += 1
                         res = json.loads(res.content)
                         tipo_orgao, created = TipoOrgao.objects.get_or_create(descricao='Outros')
-                        posicaoDoArray = 0
-                        while posicaoDoArray == len(res):
+                        posicao_do_array = 0
+                        while posicao_do_array == len(res):
                             interveniente, created = Orgao.objects.get_or_create(
                                 tipo=tipo_orgao,
-                                nome=res[posicaoDoArray]['unidadeGestora']['nome'],
+                                nome=res[posicao_do_array]['unidadeGestora']['nome'],
                                 defaults={
                                     'tipo': tipo_orgao,
-                                    'nome': res[posicaoDoArray]['unidadeGestora']['nome']
+                                    'nome': res[posicao_do_array]['unidadeGestora']['nome']
                                 }
                             )
                             concedente, created = Orgao.objects.get_or_create(
                                 tipo=tipo_orgao,
-                                nome=res[posicaoDoArray]['unidadeGestora']['orgaoVinculado']['nomeOriginal'],
+                                nome=res[posicao_do_array]['unidadeGestora']['orgaoVinculado']['nomeOriginal'],
                                 defaults={
                                     'tipo': tipo_orgao,
-                                    'nome': res[posicaoDoArray]['unidadeGestora']['orgaoVinculado']['nomeOriginal']
+                                    'nome': res[posicao_do_array]['unidadeGestora']['orgaoVinculado']['nomeOriginal']
                                 }
                             )
-                            dataInicio = datetime.strptime(res[posicaoDoArray]['dataInicioVigencia'], '%d/%m/%Y')
-                            dateFinal = datetime.strptime(res[posicaoDoArray]["dataFinalVigencia"], '%d/%m/%Y')
-                            ano = res[posicaoDoArray]["dataFinalVigencia"].split('/')[2]
+                            data_inicio = datetime.strptime(res[posicao_do_array]['dataInicioVigencia'], '%d/%m/%Y')
+                            data_final = datetime.strptime(res[posicao_do_array]["dataFinalVigencia"], '%d/%m/%Y')
+                            ano = res[posicao_do_array]["dataFinalVigencia"].split('/')[2]
                             inst, created = Instrumento.objects.get_or_create(
-                                numero_operacao=res[posicaoDoArray]['convenio']['numeroOriginal'],
+                                numero_operacao=res[posicao_do_array]['convenio']['numeroOriginal'],
                                 defaults={
                                     'entidade': entidade,
                                     'tipo':
                                         TipoInstrumento.objects.get_or_create(
                                             descricao=res[1]['tipoInstrumento']['descricao'],
-                                            defaults={'descricao': res[posicaoDoArray][
-                                                'tipoInstrumento']['descricao']})[0],
-                                    'numero_operacao': res[posicaoDoArray]['convenio']['numeroOriginal'],
-                                    'siafi_sicony': res[posicaoDoArray]['convenio']['numero'],
+                                            defaults={'descricao': res[posicao_do_array][
+                                                'tipoInstrumento']['descricao']}
+                                        )[0],
+                                    'numero_operacao': res[posicao_do_array]['convenio']['numeroOriginal'],
+                                    'siafi_sicony': res[posicao_do_array]['convenio']['numero'],
                                     'concedente': concedente,
                                     'interveniente': interveniente,
-                                    'inicio': dataInicio,
-                                    'termino': dateFinal,
+                                    'inicio': data_inicio,
+                                    'termino': data_final,
                                     'ano': int(ano),
-                                    'prorrogado': dateFinal,
-                                    'repasse': res[posicaoDoArray]['valor'],
-                                    'contrapartida': res[posicaoDoArray]['valorContrapartida'],
-                                    'total': res[posicaoDoArray]['valor'] + res[posicaoDoArray]['valorContrapartida'],
-                                    'objeto': res[posicaoDoArray]['convenio']['objeto']
+                                    'prorrogado': data_final,
+                                    'repasse': res[posicao_do_array]['valor'],
+                                    'contrapartida': res[posicao_do_array]['valorContrapartida'],
+                                    'total': res[posicao_do_array]['valor'] + res[posicao_do_array]['valorContrapartida'],
+                                    'objeto': res[posicao_do_array]['convenio']['objeto']
                                 }
                             )
+                            pagina += 1
                     else:
                         print('vazio')
                         pagina = None
@@ -77,8 +76,4 @@ def index(request):
                     sleep(10)
 
     instrument = Instrumento.objects.all()
-    return render(request,'instrumento/instrumento.html', {'entidades': instrument})
-
-    #return JsonResponse(get_entidade, safe=False)
-
-
+    return render(request, 'instrumento/instrumento.html', {'entidades': instrument})
